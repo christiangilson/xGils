cf1ac08114059314584fca3aa414230a9b35dcc3
christiangilson <christian@christiangilson.com>
2021-07-25 19:14:03 +0100

    xElo is working nicely with both duels and dual duels.

--- xElo.py
+++ xElo.py
@@ -19,6 +19,15 @@ def expected_win(r1, r2):
 
 def update_rating(R, k, P, d):
     """
+    * k represents k factor
+    * R represents the players rating (before the match)
+    * d represents the ACTUAL outcome of a match
+    * P represents the expected probability that the player would win the match
+
+    -> Therefore d-P is the difference between actual and expected
+    -> k modifies this
+    -> And you add this to the original pre-match rating as an update
+
     d = 1 = WIN
     d = 0 = LOSS
     """
@@ -27,6 +36,14 @@ def update_rating(R, k, P, d):
 
 def elo(Ra, Rb, k, d):
     """
+    A single set of Elo updates, given the outcome of a single match between two players
+    ,with the ratings for each player provided
+
+    Inputs: takes the pre-match ratings of player A and B, the k-factor, and whether A won/lost (via d=1/0)
+
+    Outputs: updated ratings for players A and B following the result
+
+
     d = 1 when player A wins
     d = 0 when player B wins
     """
@@ -52,21 +69,57 @@ def elo_sequence(things, initial_rating, k, results):
     Initialises score dictionary, and runs through sequence of pairwise results, returning final dictionary of Elo rankings
     """
 
+    # initialise the ratings of all players in the universe of players
+    # and give each of those players an initial rating (usually 100)
     dic_scores = {i:initial_rating for i in things}
+
+    # then looping through each pairwise  comparison
+    for result in results:
+
+        # to make life easier, the results should be fed in as winner / loser
+        winner, loser = result
+        # getting the pre-match scores per player from the dictionary
+        Ra, Rb = dic_scores[winner], dic_scores[loser]
+        # getting the updated scores via the elo function
+        _, _, newRa, newRb = elo(Ra, Rb, k, 1)
+        # storing those updated scores
+        dic_scores[winner], dic_scores[loser] = newRa, newRb
+
+    # outputting the dictionary of scores after a full sequence of results
+    return dic_scores
+
+
+def elo_attack_defence_sequence(things, initial_rating, k, results):
+    """
+    Initialises score dictionaries for attack and defence, and runs through sequence of pairwise results, returning final dictionaries with Elo rankings for both attack (dribblers) and defence (of dribblers)
+    """
+
+    dic_scores_attack = {i:initial_rating for i in things}
+    dic_scores_defence = {i:initial_rating for i in things}
 
     for result in results:
 
-        winner, loser = result
-        Ra, Rb = dic_scores[winner], dic_scores[loser]
-        _, _, newRa, newRb = elo(Ra, Rb, k, 1)
-        dic_scores[winner], dic_scores[loser] = newRa, newRb
+        winner, loser, dribble_outcome = result
 
-    return dic_scores
+        # winner = attacker, loser = defender
+        if dribble_outcome == 1:
+            Ra, Rb = dic_scores_attack[winner], dic_scores_defence[loser]
+            _, _, newRa, newRb = elo(Ra, Rb, k, 1)
+            dic_scores_attack[winner], dic_scores_defence[loser] = newRa, newRb
+
+        # winner = defender, loser = attacker
+        elif dribble_outcome == 0:
+            Ra, Rb = dic_scores_defence[winner], dic_scores_attack[loser]
+            _, _, newRa, newRb = elo(Ra, Rb, k, 1)
+            dic_scores_defence[winner], dic_scores_attack[loser] = newRa, newRb
+
+    return dic_scores_attack, dic_scores_defence
+
 
 
 def mElo(things, initial_rating, k, results, numEpochs):
     """
-    Randomises the sequence of the pairwise comparisons, running the Elo sequence in a random
+    Randomises the order of the pairwise comparisons, running the Elo sequence in a random
     sequence for a number of epochs
 
     Returns the mean Elo ratings over the randomised epoch sequences
@@ -80,6 +133,39 @@ def mElo(things, initial_rating, k, results, numEpochs):
 
     return pd.DataFrame(lst_outcomes).mean().sort_values(ascending=False)
 
+
+def mElo_attack_defence(things, initial_rating, k, results, numEpochs):
+    """
+    Randomises the sequence of the pairwise comparisons, running the Elo sequence in a random
+    sequence for a number of epochs
+
+    Returns the mean Elo ratings over the randomised epoch sequences
+    """
+
+    lst_outcomes_attack = []
+    lst_outcomes_defence = []
+
+    for i in np.arange(numEpochs):
+        np.random.shuffle(results)
+        dic_scores_attack, dic_scores_defence = elo_attack_defence_sequence(things, initial_rating, k, results)
+        lst_outcomes_attack.append(dic_scores_attack)
+        lst_outcomes_defence.append(dic_scores_defence)
+
+    df_attack = pd.DataFrame(lst_outcomes_attack).mean().sort_values(ascending=False).to_frame(name='eloAttack')
+    df_attack['player'] = df_attack.index
+    df_attack = df_attack.reset_index(drop=True)[['player','eloAttack']]
+
+    df_defence = pd.DataFrame(lst_outcomes_defence).mean().sort_values(ascending=False).to_frame(name='eloDefence')
+    df_defence['player'] = df_defence.index
+    df_defence = df_defence.reset_index(drop=True)[['player','eloDefence']]
+
+    df_elo = df_attack.merge(df_defence).sort_values('eloAttack', ascending=False)
+    df_elo['eloDribbleRank'] = df_elo.index+1
+
+    df_elo = df_elo.sort_values('eloDefence', ascending=False).reset_index(drop=True)
+    df_elo['eloDribbleDefenceRank'] = df_elo.index+1
+
+    return df_elo.sort_values('eloDribbleRank').reset_index(drop=True)
 
 """
 
